c71a5b635e0653658a868bd2653d3264343f3b96
Abu Bakar <abubakarccs761@gmail.com>
2022-02-27 15:29:01 +0500

    JavaScript Datatypes added"

---
+++ 01 JavaScript Fundamentals/final/script.js
@@ -0,0 +1,56 @@
+/*
+console.log(2 + 3);
+console.log(10 + 5);
+
+// VARIABLE IN JS
+
+let name = "Abbas Jan";
+console.log("Welcome");
+console.log(name);
+
+console.log("Bye");
+console.log(name);
+
+let marks = 345;
+console.log(marks);
+console.log(marks + 20);
+
+console.log(500);
+ console.log(500 + 20);
+
+
+let age = 25;
+let name = "Abu Bakar";
+let promoted = false;
+let color;
+
+console.log(name);
+console.log(age);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof promoted);
+console.log(color);
+console.log(typeof color);
+console.log(typeof null);
+
+age = "Twenty Three ";
+console.log(typeof age);
+console.log(typeof promoted);
+
+
+let age = 30;
+age = 31;
+age = 32;
+let progress;
+progress = 50;
+
+const dateOfBirth = "20 May 2000";
+// dateOfBirth = "30 May";
+// const status;
+const PI = 3.14;
+
+var name;
+
+const color = "Green";
+console.log(color);
+*/
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # the-complete-js-course
+
 All files of The Complete Javascript Course in Pashto
 
 If the download button is not working click [here](https://github.com/bakarfreelancer/the-complete-js-course/archive/refs/heads/main.zip) to download.
