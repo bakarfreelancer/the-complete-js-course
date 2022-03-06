40e96a6384a8f79eeb2dec9d3c0a11d1428971fc
Abu Bakar <abubakarccs761@gmail.com>
2022-03-06 11:30:51 +0500

    Coding-challenge-1 added

--- 01 JavaScript Fundamentals/final/script.js
+++ 01 JavaScript Fundamentals/final/script.js
@@ -54,3 +54,55 @@ var name;
 const color = "Green";
 console.log(color);
 */
+
+// Arithmetic Operators
+const currentYear = 2022;
+
+const nadeemAge = currentYear - 1980;
+const admadAge = currentYear - 1999;
+const aliAge = currentYear - 1989;
+
+console.log(nadeemAge, admadAge);
+
+const examResult = 50 + 20 + 80 + 90;
+const examAverage = examResult / 4;
+console.log(examResult, examAverage);
+
+const twoTimeThree = 2 * 3;
+console.log(twoTimeThree);
+
+const twoToThePowerThree = 2 ** 3; //2 x 2 x 2
+console.log(twoToThePowerThree);
+
+const firstName = "Abu";
+const lastName = "Bakar";
+
+const fullName = firstName + " " + lastName;
+console.log("Welcome " + fullName);
+
+// Assignment Operators
+
+let x = 30;
+x = 30 + 2; //32
+x = x - 15; //32 - 15 = 17
+x -= 15;
+
+x = x + 3; //17 + 3 = 20
+x += 3;
+
+x++; //x = x + 1;
+x--; // x = x - 1;
+console.log(x);
+
+// Logical Operators
+// ">" Greater Than
+// "<" Less Than
+// ">=" Greater or Equal
+// "<=" Less or Equal
+// "==" Equal
+// "!=" Not Equal
+
+// Nadeem Is older than Admad?
+console.log(nadeemAge > admadAge);
+
+console.log(12 != 13);
